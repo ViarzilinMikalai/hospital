88482440da5d407b84d0c1561a1e1b25e20321e9
vernik1980 <vernik3@tut.by>
2018-01-10 15:16:17 +0300

    PC commit

--- src/main/java/org/viarzilin/hospital/controller/PrescriptionsController.java
+++ src/main/java/org/viarzilin/hospital/controller/PrescriptionsController.java
@@ -2,6 +2,7 @@ package org.viarzilin.hospital.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.viarzilin.hospital.model.entity.Prescription;
 import org.viarzilin.hospital.model.service.PrescriptionsService;
 
+@Controller
 public class PrescriptionsController {
 
   private PrescriptionsService prescriptionsService;
