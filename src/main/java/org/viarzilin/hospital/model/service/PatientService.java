cb58d85bec2d5c95a075a039ba775eadffea1c8f
vernik1980 <vernik3@tut.by>
2018-01-17 12:10:55 +0300

    PC commit

---
+++ src/main/java/org/viarzilin/hospital/model/service/PatientService.java
@@ -0,0 +1,17 @@
+package org.viarzilin.hospital.model.service;
+
+import java.util.List;
+import org.viarzilin.hospital.model.entity.Patient;
+
+public interface PatientService {
+  public void addPatient(Patient patient);
+
+  public void updatePatient(Patient patient);
+
+  public void removePatient(Integer id);
+
+  public Patient getPrescriptionById(Integer id);
+
+  public List<Patient> listPatients();
+
+}
