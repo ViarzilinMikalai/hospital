ce9ec19e2ba3e361da146b732ee076cc5e5a8199
vernik1980 <vernik3@tut.by>
2018-01-20 20:07:08 +0300

    PC commit

--- src/main/java/org/viarzilin/hospital/model/service/AuthService.java
+++ src/main/java/org/viarzilin/hospital/model/service/AuthService.java
@@ -1,5 +1,17 @@
 package org.viarzilin.hospital.model.service;
 
+import java.util.List;
+import org.viarzilin.hospital.model.entity.Auth;
+
 public interface AuthService {
 
+  public void addAuth(Auth auth);
+
+  public void updateAuth(Auth auth);
+
+  public void removeAuth(Integer id);
+
+  public Auth getAuthById(Integer id);
+
+  public List<Auth> listAuthes();
 }
---
+++ src/main/java/org/viarzilin/hospital/model/service/impl/AuthServiceImpl.java
@@ -0,0 +1,47 @@
+package org.viarzilin.hospital.model.service.impl;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.viarzilin.hospital.model.dao.AuthDao;
+import org.viarzilin.hospital.model.entity.Auth;
+import org.viarzilin.hospital.model.service.AuthService;
+
+@Service
+public class AuthServiceImpl implements AuthService{
+  AuthDao authDao;
+
+  public void setAuthDao(AuthDao authDao) {
+    this.authDao = authDao;
+  }
+
+  @Override
+  @Transactional
+  public void addAuth(Auth auth) {
+    this.authDao.addAuth(auth);
+  }
+
+  @Override
+  @Transactional
+  public void updateAuth(Auth auth) {
+    this.authDao.updateAuth(auth);
+  }
+
+  @Override
+  @Transactional
+  public void removeAuth(Integer id) {
+    this.authDao.removeAuth(id);
+  }
+
+  @Override
+  @Transactional
+  public Auth getAuthById(Integer id) {
+    return this.authDao.getAuthById(id);
+  }
+
+  @Override
+  @Transactional
+  public List<Auth> listAuthes() {
+    return this.authDao.listAuthes();
+  }
+}
