5e2695a109945154b86ed049c3695cd38fa34ef6
vernik1980 <vernik3@tut.by>
2018-01-26 20:42:55 +0200

    Notebook commit 26.01.18

---
+++ src/main/java/org/viarzilin/hospital/config/WebAppInitializer.java
@@ -0,0 +1,27 @@
+package org.viarzilin.hospital.config;
+
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    @Nullable
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class};
+    }
+
+    @Nullable
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
